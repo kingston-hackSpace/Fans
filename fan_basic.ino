7875d06c454b1d3db09db16b6aab165ea555f173
CamilaColussi-KSA <c.colussi@kingston.ac.uk>
2026-04-28 10:03:14 +0100

    Update fan_basic.ino

--- fan_basic.ino
+++ fan_basic.ino
@@ -1,3 +1,4 @@
+//In this code you turn on
 
 int fanPin = 10; // PWM pin
 
@@ -6,5 +7,9 @@ void setup() {
 }
 
 void loop() {
-  analogWrite(fanPin, 200); // speed control (0–255)
+  analogWrite(fanPin, 250); // speed control (0–255), 0 = off
+  delay(2000); //wait 2 seconds
+
+  analogWrite(fanPin, 0); // speed control (0–255), 255 = max speed
+  delay(7000); //wait 7 seconds
 }
